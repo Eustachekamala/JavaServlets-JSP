e428e05e11a3a199a77f0be42cb62003cdb42ea9
eustachekamala <eustachejared25@gmail.com>
2025-04-14 12:07:05 +0300

    I did the XML annotation

---
+++ MappingServletToUrlPatterns/src/main/java/org/mapping/XMLServlet.java
@@ -0,0 +1,21 @@
+package org.mapping;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "XMLServlet", urlPatterns = "/food")
+public class XMLServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("GET METHOD HAS CALLED IN THE XMLMapping FILE");
+    }
+}
